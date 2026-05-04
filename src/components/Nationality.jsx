ad0bf394a0c0d8d31361fdefd5b9d842bd6aa801
achiniishani1993 <achiniishani1993@gmail.com>
2026-05-04 14:50:55 +0200

    added mini game

--- src/components/Nationality.jsx
+++ src/components/Nationality.jsx
@@ -1,40 +1,71 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useState } from "react";
 
 const Nationality = () => {
   const [national, setNational] = useState(null);
+  const [guess, setGuess] = useState("");
+  const [result, setResult] = useState("");
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
   const fetchdata = async () => {
     try {
-      const response = await fetch("https://api.nationalize.io?name=nathaniel");
+      const response = await fetch(
+        "https://api.nationalize.io?name=nathaniel"
+      );
       const data = await response.json();
       setNational(data);
+      setResult("");
+      setGuess("");
     } catch (error) {
       console.log("Error fetching data:", error);
     }
   };
-  //useEffect (() => {fetchdata()}, []);
 
-  const generateName = () => {
-    fetchdata();
+  const handleGuess = () => {
+    if (!national || national.country.length === 0) return;
+
+    const correct = national.country[0].country_id;
+
+    if (guess.toUpperCase() === correct) {
+      setResult("Correct!");
+      setScore(score + 1);
+    } else {
+      setResult(`Wrong! Correct answer: ${correct}`);
+    }
+
+     setShowResult(true);
   };
 
   return (
     <div>
-      <button onClick={generateName}>Generate Name</button>
+      <button onClick={fetchdata}>Generate Name</button>
+
       {!national ? (
-        <p>Click the button to load data</p>
+        <p>Click button to start game</p>
       ) : (
         <>
           <p>Name: {national.name}</p>
 
-          <ul>
+          <h3>Guess the top country Examples: US, NE, GH, NG, ID:</h3>
+
+          <input
+            type="text"
+            value={guess}
+            onChange={(e) => setGuess(e.target.value)}
+          />
+
+          <button onClick={handleGuess}>Guess</button>
+
+          <p>{result}</p>
+          <p>Score: {score}</p>
+
+          {showResult && (<ul>
             {national.country.map((c, index) => (
               <li key={index}>
                 {c.country_id} — {(c.probability * 100).toFixed(2)}%
               </li>
             ))}
-          </ul>
+          </ul>)}
         </>
       )}
     </div>
