426606e2ed943952567d70f766f119e736ee5ef1
achiniishani1993 <achiniishani1993@gmail.com>
2026-05-04 13:43:44 +0200

    Initiated package structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nationality from './components/Nationality'
+
+const App = () => {
+  return (
+    <div>
+      <Nationality/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nationality.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nationality = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Nationality
