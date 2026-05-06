f1d703d01d9f85d9f43e5e1360e4a46b325c91e1
achiniishani1993 <achiniishani1993@gmail.com>
2026-05-06 11:33:06 +0200

    add disable to button and add simple vaidation to form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,19 +2,45 @@ import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail]=useState("");
+  const [password, setPassword]=useState("");
+  const [confirmpassword, setConfirmpassword] =useState("");
+
+
+  const isDisable = username !== "" && email !== "" && password !== "" && confirmpassword !== "";
+
+  const handleSubmit = (event) =>{
+  event.preventDefault();
+  
+  if (password.length < 8){
+    alert("Password must be at least 8 characters long");
+    return;
+  }
+
+  if (password !== confirmpassword){
+    alert("Password do not matched, try again")
+    return;
+  }
+   alert("Registration successfully done...!!")
+  }
+
+
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
       <div className="p-4 bg-white shadow rounded" style={{ width: "350px" }}>
         <h4 className="text-center mb-4">Register</h4>
-
+        <form onSubmit={handleSubmit}>
         <FloatingLabel
           controlId="floatingUsername"
           label="Username"
           className="mb-3"
         >
-          <Form.Control type="text" placeholder="John" />
+          <Form.Control type="text" placeholder="John" value={username} onChange={(e)=>setUsername(e.target.value)} />
         </FloatingLabel>
 
         <FloatingLabel
@@ -22,7 +48,7 @@ const Register = () => {
           label="Email address"
           className="mb-3"
         >
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e)=>setEmail(e.target.value)}/>
         </FloatingLabel>
 
         <FloatingLabel
@@ -30,7 +56,7 @@ const Register = () => {
           label="Password"
           className="mb-3"
         >
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
         </FloatingLabel>
 
         <FloatingLabel
@@ -38,10 +64,10 @@ const Register = () => {
           label="Confirm Password"
           className="mb-3"
         >
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" value={confirmpassword} onChange={(e)=>setConfirmpassword(e.target.value)}/>
         </FloatingLabel>
 
-        <Button variant="primary" type="submit" className="w-100">
+        <Button variant="primary" type="submit" className="w-100" disabled={!isDisable}>
           Submit
         </Button>
           <p className="text-center mt-3 mb-0">
@@ -50,6 +76,7 @@ const Register = () => {
             Login
           </Link>
         </p>
+        </form>
       </div>
     </div>
   );
